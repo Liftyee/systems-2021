1fda1625a027097fe7c2f22604e4775eb3b6b2c6
Liftyee <victor.yuchen.liu@gmail.com>
2022-01-21 21:13:24 +0000

    made dfplayer work, display lazy updating

--- esp32_clock_main/src/main.cpp
+++ esp32_clock_main/src/main.cpp
@@ -2,6 +2,8 @@
 #include "time.h"
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include <Wire.h>
 #include <Arduino.h>
 #include <Fonts/FreeSans24pt7b.h>
@@ -9,33 +11,46 @@
 
 // SPI pins for the TFT display
 #define TFT_CS         5
-#define TFT_RST        17
-#define TFT_DC         16
-#define TFT_BL         0
+#define TFT_RST        4
+#define TFT_DC         0
+#define TFT_BL         15
 #define TFT_BL_CH      2
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-#define WARM_PIN       22
-#define COOL_PIN       21
+#define LIGHT_SENS     34
+#define WARM_PIN       33
+#define COOL_PIN       32
 #define WARM_CH        0
 #define COOL_CH        1
 #define PWM_RES        8
 #define PWM_FREQ       5000
 
-#define TOUCH_SENS     50
-#define SEL_P          0
-#define NEXT_P         2
+#define TOUCH_SENS     30
+#define SEL_P          3
+#define NEXT_P         4
 #define TOUCH_WAIT     1000
 
+// BME280 setup
+#define SEALEVELPRESSURE_HPA (1013.25)
+//Adafruit_BME280 bme;
 
-const char* ssid       = "ThinkPad";
-const char* password   = "6EYEBTA8";
+// WiFi setup
+const char* ssid       = "JD_HOME";
+const char* password   = "6EYEBTA8!";
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
 
-bool touchPins[10];
+// MP3 Player setup
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial DFSerial(16, 17); // RX, TX
+DFRobotDFPlayerMini MP3;
+void printDetail(uint8_t type, int value);
+
+bool touchPins[20];
 unsigned long long lastTouch[10];
 
 void gotTouch0(){
@@ -55,7 +70,7 @@ void gotTouch4(){
 }
 
 bool getTouch(int pin){
-  if (touchPins[pin] && millis() > lastTouch[pin]+TOUCH_WAIT){
+  if (touchPins[pin] && (millis() > (lastTouch[pin]+TOUCH_WAIT))){
     touchPins[pin] = false;
     lastTouch[pin] = millis();
     return true;
@@ -72,10 +87,24 @@ void printLocalTime() {
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
-//void printTextFill()
+void tftPrint(String s, int16_t x, int16_t y, uint32_t color = ST77XX_BLACK) {
+  int16_t  x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(s, x, y, &x1, &y1, &w, &h);
+  tft.fillRect(x1, y1, w, h, color);
+  tft.setCursor(x, y);
+  tft.print(s);
+}
+
+void tftPrintln(String s, uint32_t color = ST77XX_BLACK) {
+  int16_t  x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(s, tft.getCursorX(), tft.getCursorY(), &x1, &y1, &w, &h);
+  tft.fillRect(x1, y1, w, h, color);
+  tft.println(s);
+}
 
 void showTimeOnScreen() {
-  tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(0, 12);
   tft.setTextSize(1);
   tft.setTextWrap(true);
@@ -94,13 +123,18 @@ void showTimeOnScreen() {
   strftime(timedate, sizeof(timedate), "%H:%M:%S %d/%m/%Y", &timeinfo);
   strftime(shortime, sizeof(shortime), "%H:%M", &timeinfo);
   
-  tft.println(longdate);
-  tft.println(timedate);
-  tft.setFont(&FreeSans24pt7b);
-  tft.setCursor(0, 200);
-  tft.setTextSize(2);
-  tft.println(shortime);
+  tftPrintln(longdate);
+  tftPrintln(timedate);
 
+  tft.setCursor(280, 12);
+  //tft.print(bme.readTemperature());
+  tft.print("20°C");
+
+  tft.setFont(&FreeSans24pt7b);
+  tft.setTextSize(2);
+  tftPrint(shortime, 0, 200);
+
+  
 }
 
 
@@ -113,14 +147,6 @@ void showMenu() {
   tft.write("test menu");
   tft.setTextSize(1);
 }
-#include "Arduino.h"
-#include "HardwareSerial.h"
-#include "DFRobotDFPlayerMini.h"
-
-HardwareSerial mySoftwareSerial(0); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
-
 
 void setup() {
   ledcSetup(WARM_CH, PWM_FREQ, PWM_RES);
@@ -130,17 +156,18 @@ void setup() {
   ledcAttachPin(COOL_PIN, COOL_CH);
   ledcAttachPin(TFT_BL, TFT_BL_CH);
 
-  touchAttachInterrupt(T0, gotTouch0, TOUCH_SENS);
+  //touchAttachInterrupt(T0, gotTouch0, TOUCH_SENS);
   //touchAttachInterrupt(T2, gotTouch2, TOUCH_SENS);
-  //touchAttachInterrupt(T3, gotTouch3, TOUCH_SENS);
-  //touchAttachInterrupt(T4, gotTouch4, TOUCH_SENS);
+  touchAttachInterrupt(T2, gotTouch3, TOUCH_SENS);
+  touchAttachInterrupt(T4, gotTouch4, TOUCH_SENS);
 
   Serial.begin(115200);
   tft.init(240, 320);
   tft.setTextSize(1);
   tft.setRotation(3);
   tft.setFont(&FreeSans9pt7b);
-  ledcWrite(TFT_BL_CH, map(analogRead(34), 0, 4095, 1, 256));
+  tft.setTextColor(ST77XX_WHITE);
+  ledcWrite(TFT_BL_CH, map(analogRead(LIGHT_SENS), 0, 4095, 1, 256));
   //connect to WiFi
   Serial.printf("Connecting to %s ", ssid);
   tft.fillScreen(ST77XX_BLACK);
@@ -162,30 +189,49 @@ void setup() {
   WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
   
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
+  Serial.println("BME280 test");
+
+  // unsigned status;
   
-  mySoftwareSerial.begin(9600, SERIAL_8N1, 3, 1);
+  // // default settings
+  // status = bme.begin();  
+  // // You can also pass in a Wire library object like &Wire2
+  // // status = bme.begin(0x76, &Wire2)
+  // if (!status) {
+  //     Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+  //     Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+  //     Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+  //     Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+  //     Serial.print("        ID of 0x60 represents a BME 280.\n");
+  //     Serial.print("        ID of 0x61 represents a BME 680.\n");
+  //     while (1) delay(10);
+  // }
+
+  tft.fillScreen(ST77XX_BLACK);
+  
+  DFSerial.begin(9600);
 
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
 
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+  if (!MP3.begin(DFSerial)) {  //Use softwareSerial to communicate with mp3.
+    tft.print("DFPlayer not connected!");
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-    while(true);
+    //while(true);
   }
   Serial.println(F("DFPlayer Mini online."));
 
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
-  myDFPlayer.play(1);  //Play the first mp3
+  MP3.volume(30);  //Set volume value. From 0 to 30
+  MP3.play(3);  //Play the first mp3
 }
 unsigned long long lastTime;
 
 int lightState = 256;
 void loop() {
+  Serial.print(touchRead(2));
   if (!inMenu) {
     if (millis() >= lastTime + 1000) {
       lastTime = millis();
@@ -193,11 +239,18 @@ void loop() {
       showTimeOnScreen();
     }
     if (getTouch(SEL_P)) {
+      Serial.println("SEL button pressed!");
       inMenu = true;
+      tft.fillScreen(ST77XX_BLACK);
       showMenu();
     }
   } else {
     showMenu();
+    if (getTouch(SEL_P)) {
+      Serial.println("SEL button pressed!");
+      inMenu = false;
+      tft.fillScreen(ST77XX_BLACK);
+    }
   }
 
   ledcWrite(TFT_BL_CH, map(analogRead(34), 0, 4095, 1, 256));
@@ -230,8 +283,8 @@ void loop() {
   delay(100);
 
   //dacWrite(25, 128);
-  if (myDFPlayer.available()) {
-    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  if (MP3.available()) {
+    printDetail(MP3.readType(), MP3.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
 }
 
